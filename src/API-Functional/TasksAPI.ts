2904c2867d054b99171730e48ffc943c93cf2fc9
FelixT <felixtekhada@gmail.com>
2023-03-26 12:46:38 +0200

    tested working with api+tasks

---
+++ src/API-Functional/TasksAPI.ts
@@ -0,0 +1,32 @@
+import {instance} from "./ToDoListsAPI";
+
+export type OneTaskType = {
+    id: string
+    title: string
+    description: string
+    todoListId: string
+    order: number
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    addedDate: string
+}
+
+export type ResponseTasksType = {
+    items: OneTaskType[]
+    totalCount: number
+    error: null | string
+}
+
+
+export const tasksAPI = {
+    //axios заменил на созданый instance, как он работает описал в листАПИ
+
+    getTasks(listID: string) {
+        return instance.get<ResponseTasksType>(`/todo-lists/${listID}/tasks?count=20`).then(r => r.data.items)
+    },
+    postTask(listID: string, taskTitle: string) {
+        return instance.post<ResponseTasksType>(`/todo-lists/${listID}/tasks`, {title: taskTitle})
+    }
+}
--- src/API-Functional/ToDoListsAPI.ts
+++ src/API-Functional/ToDoListsAPI.ts
@@ -1,21 +1,45 @@
 import axios from "axios";
 
-export const setting = {
+export const settings = {
     withCredentials: true,
     headers: {
         'API-KEY': '70ba2ec4-2ba1-411d-b8a2-7b24b30ac2ca'   //без ключа тоже работало
     }
 }
 
+export const instance = axios.create({ //позволяет создать шаблон для запросов, потом пишем вместо axios=>instance
+    baseURL: 'https://social-network.samuraijs.com/api/1.1', // вставится втоматом, пишет только то что будет после '/toDoLists/tasks'...
+    ...settings  //тоже поставится автоматом
+})
+
+export type OneListAPIType = {
+    addedDate: string
+    id: string
+    order: number
+    title: string
+}
+
+export type ResponseListAPIType<D={}> = {
+    resultCode: number
+    messages: string[],
+    fieldsErrors: []
+    data: D
+}
+
+
 export const toDoListsAPI = {
+    //везде заменил на инстанс, как он работает описал выше
     getLists() {
-        return axios.get('https://social-network.samuraijs.com/api/1.1//todo-lists', setting)
-            .then(r => r.data)
+        return instance.get<OneListAPIType[]>(`/todo-lists`).then(r => r.data)
     },
     postList(title: string) {
-        return axios.post('https://social-network.samuraijs.com/api/1.1//todo-lists', {title: title}, setting)
+        return instance.post<ResponseListAPIType<{ item: OneListAPIType }>>(`/todo-lists`, {title: title})
     },
     deleteList(listID: string) {
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1//todo-lists/${listID}`, setting)
-    }
+        return instance.delete<ResponseListAPIType>(`/todo-lists/${listID}`)
+    },
+    renameList(listID: string, newListTitle: string) {
+        return instance.put<ResponseListAPIType>(`/todo-lists/${listID}`, {title: newListTitle})
+    },
+
 }
